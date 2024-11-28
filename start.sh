50925bf6bfaaf41f88f0e66523ff16b730641801
Waqas Khan <118829654+mwaqaskhan2002@users.noreply.github.com>
2024-11-29 02:50:41 +0500

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Define the URL to download ollama.exe from GitHub
-OLLAMA_URL="https://github.com/mwaqaskhan2002/Ollama-FastAPI/releases/download/v0.4.4/ollama.exe"
+OLLAMA_URL="https://github.com/mwaqaskhan2002/Ollama-FastAPI/releases/download/v0.4.4/ollama.linux"
 
 # Download ollama.exe if it doesn't exist
-if [ ! -f "ollama.exe" ]; then
-  echo "Downloading ollama.exe..."
-  curl -L -o ollama.exe $OLLAMA_URL
+if [ ! -f "ollama" ]; then
+  echo "Downloading ollama..."
+  curl -L -o ollama $OLLAMA_URL
 fi 
 
 # Make ollama.exe executable
-chmod +x ollama.exe
+chmod +x ollama
 
 # Start Ollama server
 echo "Starting Ollama server..."
-./ollama.exe serve &
+./ollama serve &
 
 # Wait for Ollama server to start
 sleep 5
 
 # Pull the model
 echo "Pulling model llama3.2..."
-./ollama.exe pull llama3.2
+./ollama pull llama3.2
 
 # Start FastAPI
 echo "Starting FastAPI..."
